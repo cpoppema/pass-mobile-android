491923eb25acdf79a0a7fbf9165cfd5225c82c72
cpoppema <cpoppema@gmail.com>
2016-05-09 23:00:14 +0200

    Some Android 6 findings.

--- app/src/main/java/mobile/android/pass/utils/CircularImageView.java
+++ app/src/main/java/mobile/android/pass/utils/CircularImageView.java
@@ -16,6 +16,10 @@ public class CircularImageView {
         RoundedBitmapDrawable drawable = RoundedBitmapDrawableFactory.create(v.getResources(), bitmap);
         drawable.setCircular(true);
         v.setBackground(drawable);
-        bitmap.recycle();
+
+        // Causing these log entries in 6.0:
+        // Called getWidth() on a recycle()'d bitmap! This is undefined behavior!
+        // Called getHeight() on a recycle()'d bitmap! This is undefined behavior!
+//        bitmap.recycle();
     }
 }
